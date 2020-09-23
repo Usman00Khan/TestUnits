10b80924bdbeea6cebc85e5e8c52bf6a0ef66367
Usman Khan <uk80718@gmail.com>
2020-09-23 12:17:46 +0530

    Added Test RunnerClass

--- src/RunnerClass.java
+++ src/RunnerClass.java
@@ -4,7 +4,7 @@ import org.junit.runner.notification.Failure;
 
 public class RunnerClass {
     public static void main(String[] args) {
-        Result res = JUnitCore.runClasses(TestCalculate.class, VoidMethodClass.class);
+        Result res = JUnitCore.runClasses(TestCalculate.class, TestVoidMethodClass.class);
         for(Failure f: res.getFailures()){
             System.out.println(f.toString());
         }
