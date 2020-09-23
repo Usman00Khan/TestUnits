6de87df65e6b2c22b5071acc218688ea4f413434
Usman Khan <uk80718@gmail.com>
2020-09-23 10:33:38 +0530

    Added after and before function

--- src/Calculate.java
+++ src/Calculate.java
@@ -2,4 +2,9 @@ public class Calculate {
     public int sum(int a, int b){
         return a+b;
     }
+
+    public int diff(int a, int b)
+    {
+        return a - b;
+    }
 }
--- src/TestCalculate.java
+++ src/TestCalculate.java
@@ -1,18 +1,45 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestCalculate {
+    @Before
+    public void beforeTest() {
+        System.out.println("This runs before every Test.");
+    }
+
+    @After
+    public void afterTest() {
+        System.out.println("This runs After every Test.");
+    }
+
     @Test
-    public void testSum(){
+    public void testSum() {
         Calculate c = new Calculate();
-        int a =5;
-        int b= 6;
+        int a = 5;
+        int b = 6;
         int exp = 11;
+        int actual = c.sum(a, b);
 
-        int actual = c.sum(a,b);
-
+        System.out.println("Sum test running");
         Assert.assertEquals(exp, actual);
 
 
     }
+
+    @Test
+    public void testDiff() {
+        Calculate c = new Calculate();
+        int a = 5;
+        int b = 6;
+        int exp = -1;
+        int actual = c.diff(a, b);
+
+        System.out.println("Difference test is running");
+        Assert.assertEquals(exp, actual);
+
+
+    }
+
 }
